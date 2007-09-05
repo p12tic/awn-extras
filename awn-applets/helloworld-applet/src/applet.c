94066fdfadd159c7d19bf97554a510ed8ddcb5b0
Neil Jagdish Patel <njp@hp-laptop>
2007-09-05 20:26:43 +0100

    Added a simple helloworld applet thanks to mosburger and hayjes

---
+++ awn-applets/helloworld-applet/src/applet.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2007 Mike Desjardins
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include <libawn/awn-applet.h>
+#include "config.h"
+
+#include "helloworldapplet.h"
+
+AwnApplet*
+awn_applet_factory_initp ( gchar* uid, gint orient, gint height )
+{
+	AwnApplet *applet;
+	HelloWorld *helloworld;
+	
+	printf ("avant init\n");
+	applet = awn_applet_new( uid, orient, height );
+	gtk_widget_set_size_request (GTK_WIDGET (applet), awn_applet_get_height (applet) * 2, awn_applet_get_height (applet) );
+	helloworld = helloworld_applet_new(applet);
+	printf ("apres init\n");
+	
+	return applet;
+}
+

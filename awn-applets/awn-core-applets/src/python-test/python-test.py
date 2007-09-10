f4ef98d1b67937b5f0b1d9160e4dff4cfb3eeaf9
Julien Lavergne <gilir@ubuntu>
2007-09-10 10:16:09 +0200

    Clean debian dir Add python-test of the trunk

--- awn-applets/awn-core-applets/src/python-test/python-test.py
+++ awn-applets/awn-core-applets/src/python-test/python-test.py
@@ -5,8 +5,6 @@ import pygtk
 import gtk
 from gtk import gdk
 import awn
-import empathy
-import empathygtk
 
 class App (awn.AppletSimple):
   def __init__ (self, uid, orient, height):
@@ -15,15 +13,12 @@ class App (awn.AppletSimple):
     self.theme = gtk.IconTheme ()
     icon = self.theme.load_icon ("gtk-apply", height, 0)
     #icon = gdk.pixbuf_new_from_file ("/home/njp/Projects/test.png")
-    self.set_icon (icon)
+    self.set_temp_icon (icon)
     self.title = awn.awn_title_get_default ()
     self.dialog = awn.AppletDialog (self)
     button = gtk.Button (stock="gtk-apply")
     self.dialog.add (button)
     button.show_all ()
-    store = empathygtk.ContactListStore(contact_manager)
-    view = empathygtk.ContactListView(store)
-    self.dialog.add (view)
     self.connect ("button-press-event", self.button_press)
     self.connect ("enter-notify-event", self.enter_notify)
     self.connect ("leave-notify-event", self.leave_notify)
@@ -41,13 +36,13 @@ class App (awn.AppletSimple):
   def enter_notify (self, widget, event):
     self.title.show (self, "Test python applet")
     icon = self.theme.load_icon ("gtk-apply", self.height, 0)
-    self.set_icon (icon)
+    self.set_temp_icon (icon)
     print "show title"
 
   def leave_notify (self, widget, event):
-    icon = self.theme.load_icon ("gtk-cancel", self.height, 0)
-    self.set_icon (icon)
     self.title.hide (self)
+    icon = self.theme.load_icon ("gtk-cancel", self.height, 0)
+    self.set_temp_icon (icon)
     print "hide title"
 
 if __name__ == "__main__":

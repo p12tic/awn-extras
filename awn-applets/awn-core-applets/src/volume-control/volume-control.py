89c1cfd54b0f399d2ddb15070d77cb0b5c1cc12b
Julien Lavergne <gilir@ubuntu>
2007-09-22 00:02:21 +0200

    * volume-control applet : New version (0.3) by nazrat  - scrolling the mousewheel above the icon changes the volume.  - redesigned popup-dialog with the long awaited slider to change the volume.  - middle mouse click on the icon mutes the output (awesome job done by jawbreaker) (there is also a button for this in the popup-dialog)  - right click on the icon opens the gnome volume control (now without stopping the applet from working)  - complete new icon theme plus the ability to choose your own icon theme (tango theme still included)

--- awn-applets/awn-core-applets/src/volume-control/volume-control.py
+++ awn-applets/awn-core-applets/src/volume-control/volume-control.py
@@ -1,10 +1,13 @@
 """
 A python Applet for the avant-windows-navigator
-to control the volume and the audio player.
+to control the volume.
 
-Version 0.1.
+Version 0.3.
 
-Richard Beyer September 2007
+Richard "nazrat" Beyer
+Jeff "Jawbreaker" Hubbard
+
+September 2007
 """
 
 #!/usr/bin/python
@@ -14,21 +17,29 @@ import pygtk
 import gtk
 from gtk import gdk
 import awn
+#Module to control the alsamixer.
 try:
     import alsaaudio as alsa
 except:
     print "you need to install the alsaaudio-python module !"
     sys.exit(1)
-try:
-    import commands
-except:
-    print "you need to install the alsaaudio-python module !"
-    sys.exit(1)    
+#Module to get gconf keys.
 try:
     import gconf
 except:
     print "you need to install the python-gconf module !"
-    sys.exit(1)
+    sys.exit(1)     
+#Get the gconf-key for the main audio device.
+try:
+  key = '/desktop/gnome/sound/default_mixer_tracks'
+  client = gconf.client_get_default()
+  channels = client.get_list(key, gconf.VALUE_STRING)
+  control_channel = channels[0]
+except:
+  control_channel = 'PCM'
+   
+
+__tmp_mute_level = 0
 
 class VolumeApp (awn.AppletSimple):
 
@@ -36,10 +47,11 @@ class VolumeApp (awn.AppletSimple):
   try:
     key = '/desktop/gnome/sound/default_mixer_tracks'
     client = gconf.client_get_default ()
-    values = client.get_list(key, gconf.VALUE_STRING)
-    ctrl_chan = values[0]
+    channels = client.get_list(key, gconf.VALUE_STRING)
+    control_channel = channels[0]
   except:
-    ctrl_chan = 'PCM'
+    control_channel = 'PCM'
+ 
 
   def __init__ (self, uid, orient, height):
     awn.AppletSimple.__init__ (self, uid, orient, height)
@@ -49,12 +61,22 @@ class VolumeApp (awn.AppletSimple):
     self.title = awn.awn_title_get_default ()
 
     
-    #Create the pop-up dialog and the buttons within.
+    #Create the pop-up dialog and the buttons/sliders within.
     self.dialog = awn.AppletDialog (self)
-    dialogcontainer = gtk.VBox()
-    controls_container = gtk.HBox ()
-    volume_container = gtk.HBox ()
-
+    dialogcontainer = gtk.HBox()
+    dialogcontainer.set_spacing(6)
+    #controls_container = gtk.VBox ()
+    volume_container = gtk.VBox ()
+    volume_container.set_spacing(6)
+    
+    global volume_slider
+    volume_slider = gtk.VScale()
+    volume_slider.set_range(0, 100)
+    volume_slider.set_digits(0)
+    volume_slider.set_inverted(True)
+    volume_slider.set_value(alsa.Mixer(self.control_channel).getvolume()[0])
+    
+    """  
     button_play = gtk.Button ()
     button_play.set_relief(gtk.RELIEF_NONE)
     button_play_image = gtk.Image()
@@ -72,31 +94,37 @@ class VolumeApp (awn.AppletSimple):
     button_next_image = gtk.Image()
     button_next_image.set_from_file("/usr/share/icons/Tango/24x24/actions/gtk-media-next-ltr.png")
     button_next.set_image(button_next_image)
+    """
 
     button_volume_up = gtk.Button ()
     button_volume_up.set_relief(gtk.RELIEF_NONE)
     button_volume_up_image = gtk.Image()
-    button_volume_up_image.set_from_file("/usr/share/icons/Tango/24x24/actions/add.png")
+    button_volume_up_image.set_from_stock('gtk-go-up', gtk.ICON_SIZE_LARGE_TOOLBAR)
     button_volume_up.set_image(button_volume_up_image)
 
     button_volume_down = gtk.Button ()
     button_volume_down.set_relief(gtk.RELIEF_NONE)
     button_volume_down_image = gtk.Image()
-    button_volume_down_image.set_from_file("/usr/share/icons/Tango/24x24/actions/remove.png")
+    button_volume_down_image.set_from_stock('gtk-go-down', gtk.ICON_SIZE_LARGE_TOOLBAR)
     button_volume_down.set_image(button_volume_down_image)
     
+    self.button_volume_mute = gtk.ToggleButton()
+    self.button_volume_mute.set_relief(gtk.RELIEF_NONE)
+    self.button_volume_mute_image = gtk.Image()
+    self.button_volume_mute_image.set_from_file("/usr/share/icons/Tango/24x24/status/audio-volume-muted.png")
+    self.button_volume_mute.set_image(self.button_volume_mute_image)    
+    
     knob_image = gtk.Image()
-    knob_image.set_from_file("/usr/share/icons/gnome/48x48/apps/gnome-settings-sound.png")
+    knob_image.set_from_file("/usr/share/icons/Tango/24x24/apps/preferences-desktop-sound.png")
 
     #Arrange the buttons.
     volume_container.add (knob_image)
     volume_container.add (button_volume_up)
     volume_container.add (button_volume_down)
-    controls_container.add (button_previous)
-    controls_container.add (button_play)
-    controls_container.add (button_next)
+    volume_container.add (self.button_volume_mute)
     dialogcontainer.add (volume_container)
-    dialogcontainer.add (controls_container)
+    dialogcontainer.add (volume_slider)
+    
 
     self.dialog.add (dialogcontainer)
     #controls_container.show_all ()
@@ -107,26 +135,45 @@ class VolumeApp (awn.AppletSimple):
     self.connect ("enter-notify-event", self.enter_notify)
     self.connect ("leave-notify-event", self.leave_notify)
     self.dialog.connect ("focus-out-event", self.dialog_focus_out)
+    """
     button_previous.connect ("button-press-event", self.button_previous_press)
     button_play.connect ("button-press-event", self.button_play_press)
     button_next.connect ("button-press-event", self.button_next_press)
+    """
     button_volume_up.connect ("button-press-event", self.button_volume_up_press)
     button_volume_down.connect ("button-press-event", self.button_volume_down_press)
+    volume_slider.connect ("value-changed", self.volume_slider_changed)
+    self.button_volume_mute.connect ("button-press-event", self.button_volume_mute_toggled)
 
 
   #Set the applet-icon based on the actual volume.
+  #Change "Black" in line 153 to the folder name of your prefered icon theme (e.g. "Tango").
   def set_applet_icon (self):
-    currentvolume = alsa.Mixer(self.ctrl_chan).getvolume()[0]
+    currentvolume = alsa.Mixer(self.control_channel).getvolume()[0]
+    theme = "Black"
+    themepath = os.path.abspath(os.path.dirname(__file__)) + "/Themes/" + theme + "/"
     if currentvolume > 60 :
-      icon = gdk.pixbuf_new_from_file ("/usr/share/icons/Tango/scalable/status/audio-volume-high.svg")
+      icon = gdk.pixbuf_new_from_file (themepath + "audio-volume-high.svg")
     elif currentvolume > 25 :
-      icon = gdk.pixbuf_new_from_file ("/usr/share/icons/Tango/scalable/status/audio-volume-medium.svg")
+      icon = gdk.pixbuf_new_from_file (themepath + "audio-volume-medium.svg")
     elif currentvolume > 0 :
-      icon = gdk.pixbuf_new_from_file ("/usr/share/icons/Tango/scalable/status/audio-volume-low.svg")
+      icon = gdk.pixbuf_new_from_file (themepath + "audio-volume-low.svg")
     else:
-      icon = gdk.pixbuf_new_from_file ("/usr/share/icons/Tango/scalable/status/audio-volume-muted.svg")  
+      icon = gdk.pixbuf_new_from_file (themepath + "audio-volume-muted.svg")  
     self.set_temp_icon (icon)
     
+  #Set the mute icon based on the actual volume.
+  def set_mute_icon (self):
+    currentvolume = alsa.Mixer(control_channel).getvolume()[0]
+    if currentvolume > 60 :
+      self.button_volume_mute_image.set_from_file("/usr/share/icons/Tango/24x24/status/audio-volume-high.png")
+    elif currentvolume > 25 :
+      self.button_volume_mute_image.set_from_file("/usr/share/icons/Tango/24x24/status/audio-volume-medium.png")
+    elif currentvolume > 0 :
+      self.button_volume_mute_image.set_from_file("/usr/share/icons/Tango/24x24/status/audio-volume-low.png")
+    else:
+      self.button_volume_mute_image.set_from_file("/usr/share/icons/Tango/24x24/status/audio-volume-muted.png") 
+ 
  
   #When the mouse leaves the dialog it disappears.
   def dialog_focus_out (self, widget, event):
@@ -138,13 +185,15 @@ class VolumeApp (awn.AppletSimple):
   #should pause/continue play, and when right-clicked the mixer-control should appear.
   def button_press (self, widget, event):
     if event.button == 1:
-        print "leftmouse clicked -> open controls"
+        #print "leftmouse clicked -> open controls"
+        self.dialog.show_all ()
     if event.button == 2:
-        print "middlemouse clicked (not yet implemented!)"
+        #print "middlemouse clicked -> mute/unmute"
+        self.volume_mute_toggle()
     if event.button == 3:
-        print "rightmouse clicked -> run gnome-volume-control"
-        commands.getstatusoutput("gnome-volume-control")
-    self.dialog.show_all ()
+        #print "rightmouse clicked -> run gnome-volume-control"
+        os.popen("gnome-volume-control &")
+        #self.player_dialog()
     self.title.hide (self)
 
 
@@ -156,15 +205,14 @@ class VolumeApp (awn.AppletSimple):
     elif event.direction == gtk.gdk.SCROLL_DOWN:
       #print "wheel down" 
       self.volume_down()
-    volumestring = "Volume: " + str(alsa.Mixer(self.ctrl_chan).getvolume()[0]) + "%"
+    volumestring = "Volume: " + str(alsa.Mixer(self.control_channel).getvolume()[0]) + "%"
     self.title.show (self, volumestring)
 
 
   #When "mouse over applet" the current volume should appear.
   def enter_notify (self, widget, event):
-    volumestring = "Volume: " + str(alsa.Mixer(self.ctrl_chan).getvolume()[0]) + "%"
+    volumestring = "Volume: " + str(alsa.Mixer(self.control_channel).getvolume()[0]) + "%"
     self.title.show (self, volumestring)
-    #self.set_temp_icon (icon)
 
 
   #When the mouse leaves the applet title disappears.
@@ -172,6 +220,15 @@ class VolumeApp (awn.AppletSimple):
     self.title.hide (self)
     #print "hide title"
 
+  
+  #Moving the slider up and down changes the volume.
+  def volume_slider_changed (self, widget):#, vol):
+    volume = int(volume_slider.get_value())
+    #print "slider changed to " + str(volume)
+    alsa.Mixer(self.control_channel).setvolume(volume)
+    self.set_applet_icon ()
+  
+  """
   #Clicking the audio-control-buttons should control the prefered music player application,
   #preferably through DBus.  
   def button_previous_press (self, widget, event):
@@ -182,9 +239,9 @@ class VolumeApp (awn.AppletSimple):
 
   def button_next_press (self, widget, event):
     print "button_next clicked!!! (not yet implemented!)"
+  """
 
-
-  #Clicking the volume-control-buttons should increase/decrease the volume.
+  #Clicking the volume-control-buttons should increase/decrease/mute the volume.
   def button_volume_up_press (self, widget, event):
     #print "button_volume_up clicked."
     self.volume_up()
@@ -193,27 +250,47 @@ class VolumeApp (awn.AppletSimple):
     #print "button_volume_down clicked."
     self.volume_down()
 
+  def button_volume_mute_toggled (self, widget, event):
+    #print "mute-button clicked."
+    self.volume_mute_toggle()
 
-  #The methods to increase/decrease the volume.
+
+  #The methods to increase/decrease/mute the volume.
   def volume_up(self):
-    volume = alsa.Mixer(self.ctrl_chan).getvolume()[0]
+    volume = alsa.Mixer(self.control_channel).getvolume()[0]
     # if the volume is under 97, increase the volume for one step.
     if volume < 97:
-      alsa.Mixer(self.ctrl_chan).setvolume(volume+4)
+      alsa.Mixer(self.control_channel).setvolume(volume+4)
+      volume_slider.set_value (volume+4)
     # if the volume is over 97, set it to 100.
     else:
-      alsa.Mixer(self.ctrl_chan).setvolume(100)
-    self.set_applet_icon ()  
+      alsa.Mixer(self.control_channel).setvolume(100)
+      volume_slider.set_value (100)
+    self.set_applet_icon ()
+    self.set_mute_icon() 
 
   def volume_down(self):
-    volume = alsa.Mixer(self.ctrl_chan).getvolume()[0]
+    volume = alsa.Mixer(self.control_channel).getvolume()[0]
     # if the volume is over 3, decrease it's value for one step.
     if volume > 3:
-      alsa.Mixer(self.ctrl_chan).setvolume(volume-4)
+      alsa.Mixer(self.control_channel).setvolume(volume-4)
+      volume_slider.set_value (volume-4)
     # if the volume is under 3, set it to 0 and mute it.
     elif volume > 0:
-      alsa.Mixer(self.ctrl_chan).setvolume(0)
-    self.set_applet_icon ()  
+      alsa.Mixer(self.control_channel).setvolume(0)
+      volume_slider.set_value (0)
+    self.set_applet_icon ()
+    self.set_mute_icon() 
+
+  def volume_mute_toggle(self):
+    volume = alsa.Mixer(control_channel).getvolume()[0]
+    if volume > 0:
+      self.__tmp_mute_level = volume
+      alsa.Mixer(control_channel).setvolume(0)
+    else:
+      alsa.Mixer(control_channel).setvolume(self.__tmp_mute_level)
+    self.set_mute_icon()
+    self.set_applet_icon ()
 
 
 #Main loop

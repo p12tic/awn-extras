435d85feb20352d7a8f536bc8f24dad81fe8a369
moonbeam <rcryderman@gmail.com>
2007-12-27 22:03:23 -0500

    shiny-switcher 	-fixed copyright attribution

--- awn-applets/awn-extras-applets/src/shiny-switcher/shinyswitcherapplet.c
+++ awn-applets/awn-extras-applets/src/shiny-switcher/shinyswitcherapplet.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007 Mike Desjardins
+ * Copyright (c) 2007 Rodney Cryderman (rcryderman@gmail.com) 
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public

eac4b28d87648c40cb9433d4897b1c305b9eaf7f
moonbeam <rcryderman@gmail.com>
2008-01-22 21:47:31 -0500

    Taskmand-applet 	-use the dbus launcher position interface

--- src/Taskmand-applet/applet.c
+++ src/Taskmand-applet/applet.c
@@ -20,6 +20,46 @@
 
 #include <gtk/gtk.h>
 #include <libawn/awn-applet-simple.h>
+#include "client-bindings.h"
+
+
+void do_dbus_stuff(const char * uid)
+{
+
+    /* Somewhere in the code, we want to execute EchoString remote method */
+    DBusGProxy *proxy;
+    DBusGConnection *connection;
+    GError *error = NULL;
+    
+    connection = dbus_g_bus_get (DBUS_BUS_SESSION, &error);
+    if (connection == NULL)
+    {
+        g_warning("Unable to connect to dbus: %sn", error->message);
+        g_error_free (error);
+        /* Basically here, there is a problem, since there is no dbus :) */
+        return;
+    }
+
+    /* This won't trigger activation! */
+    proxy = dbus_g_proxy_new_for_name (connection,
+            "org.awnproject.taskmand",
+            "/org/awnproject/taskmand",
+            "org.awnproject.taskmand");
+    /* The method call will trigger activation, more on that later */
+    if (!org_awnproject_taskmand_launcher__position(proxy, uid, &error))
+    {
+        /* Method failed, the GError is set, let's warn everyone */
+        g_warning ("Woops remote method failed: %s", error->message);
+        g_error_free (error);
+        return;
+    }
+
+    /* Cleanup */ 
+    g_object_unref (proxy);
+
+    /* The DBusGConnection should never be unreffed, it lives once and is shared amongst the process */
+    
+}
 
 static void _realized(GtkWidget *widget,AwnApplet *applet)
 {
@@ -41,6 +81,7 @@ AwnApplet* awn_applet_factory_initp ( gchar* uid, gint orient, gint height )
     gtk_widget_set_size_request (GTK_WIDGET (applet),height-2,height-2);
     awn_applet_simple_set_temp_icon (AWN_APPLET_SIMPLE (applet),icon);
     g_signal_connect_after(G_OBJECT (applet), "realize", G_CALLBACK (_realized), applet);    
+    do_dbus_stuff(uid);
     return applet;
 }
 

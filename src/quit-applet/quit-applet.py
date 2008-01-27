1a0f57447f8a55c77cf80493453e5b7636b12efb
moonbeam <rcryderman@gmail.com>
2008-01-27 16:40:41 -0500

    quit-applet 	-applied patch (arvind):  http://launchpadlibrarian.net/11586405/quit-applet.patch 	-bug:  https://bugs.launchpad.net/awn-extras/+bug/186387 	-adds a preference-window with right click on the logout icon. This creates a popup that enables users to enter the logout-command. The command is stored in GConf, in the same gconf-dir where the icon-setting is stored.  By default, it uses `gnome-session-save --kill` so nothing will break for current users.

--- src/quit-applet/quit-applet.py
+++ src/quit-applet/quit-applet.py
@@ -28,8 +28,10 @@ import gconf
 class App (awn.AppletSimple):
     def __init__ (self, uid, orient, height):
         self.location = __file__.replace('quit-applet.py','')
-        self.keylocation = "/apps/avant-window-navigator/applets/QuitApplet/"
+        self.keylocation = "/apps/avant-window-navigator/applets/QuitApplet"
+        self.client = gconf.client_get_default()
         self.load_keys()
+	self.client.notify_add(self.keylocation, self.config_event)
         awn.AppletSimple.__init__ (self, uid, orient, height)
         self.height = height
         self.theme = gtk.IconTheme ()
@@ -43,19 +45,39 @@ class App (awn.AppletSimple):
         self.connect ("button-press-event", self.button_press)
         self.connect ("enter-notify-event", self.enter_notify)
         self.connect ("leave-notify-event", self.leave_notify)
+
+	# Setup popup menu
+	self.popup_menu = gtk.Menu()
+	pref_item = gtk.ImageMenuItem(stock_id=gtk.STOCK_PREFERENCES)
+	self.popup_menu.append(pref_item)
+	pref_item.connect_object("activate", self.pref_callback, self)
+	pref_item.show()
+
+    def pref_callback(self, widget):
+	window = PreferenceDialog(self)
+	window.set_type_hint(gtk.gdk.WINDOW_TYPE_HINT_DIALOG)
+	window.set_destroy_with_parent(True)
+	window.show_all()
+
     def button_press (self, widget, event):
-        self.title.hide (self)
-        os.system("gnome-session-save --kill")      
+	self.title.hide(self)
+	if event.button == 3:
+	    # right click
+	    self.popup_menu.popup(None, None, None, event.button, event.time)
+	else:
+            os.system(self.command)
     #def dialog_focus_out (self, widget, event):
     #  print ""
     def enter_notify (self, widget, event):
         self.title.show (self, "Quit/Logout?")
     def leave_notify (self, widget, event):
         self.title.hide (self)
+    def config_event(self, gconf_client, *args, **kwargs):
+	self.load_keys()
     def load_keys(self):
-        self.client                         = gconf.client_get_default()
         #<Name of Variable> = self.key_control (<Name of Key>,<Default Value>)
-        self.icon_location                       = self.key_control ("IconLocation",self.location + "icons/application-exit.svg")
+        self.icon_location                       = self.key_control ("/IconLocation",self.location + "icons/application-exit.svg")
+	self.command				 = self.key_control ("/LogoutCommand", "gnome-session-save --kill")
 
     def key_control(self,keyname,default):
         keylocation_with_name               = self.keylocation + keyname
@@ -67,6 +89,41 @@ class App (awn.AppletSimple):
         except NameError:
             var                             = default
         return var  
+
+class PreferenceDialog(gtk.Window):
+    def __init__(self,applet):
+	super(PreferenceDialog, self).__init__(gtk.WINDOW_TOPLEVEL)
+	self.applet = applet
+
+	self.set_title("Preferences")
+	vbox = gtk.VBox(True, 0)
+	self.add(vbox)
+
+	vbox1 = gtk.VBox(True, 0)
+	label1 = gtk.Label("Logout command:")
+	self.logout_command = gtk.Entry(max=0)
+	self.logout_command.set_text(applet.command)
+	vbox1.pack_start(label1)
+	vbox1.pack_end(self.logout_command)
+	vbox.pack_start(vbox1,True,False,2)
+
+	hbox4 = gtk.HBox(True, 0)
+	ok = gtk.Button(stock=gtk.STOCK_OK)
+	ok.connect("clicked", self.ok_button, "ok")
+	hbox4.add(ok)
+	cancel = gtk.Button(stock=gtk.STOCK_CANCEL)
+	cancel.connect("clicked", self.cancel_button, "cancel")
+	hbox4.add(cancel)
+	vbox.pack_end(hbox4,True,False,2)
+
+    def ok_button(self, widget, event):
+	self.applet.client.set_string(self.applet.keylocation + "/LogoutCommand", self.logout_command.get_text().strip())
+	self.destroy()
+
+    def cancel_button(self, widget, event):		
+	self.destroy()
+
+
 if __name__ == "__main__":
     awn.init (sys.argv[1:])
     applet = App (awn.uid, awn.orient, awn.height)

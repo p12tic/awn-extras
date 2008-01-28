374524871fe34aca7076a2beca835089d0421859
Mark Lee <bzr@lazymalevolence.com>
2008-01-28 14:06:37 -0800

    post-AWNLib-move fixes

--- src/mail/mail.py
+++ src/mail/mail.py
@@ -19,7 +19,7 @@
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #       MA 02110-1301, USA.
 
-import AWNLib
+from awn.extras import AWNLib
 import gobject
 import gtk
 from gtk import gdk

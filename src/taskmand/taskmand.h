ab3b19b028192e9057d5a441930453b8973480cb
moonbeam <rcryderman@gmail.com>
2008-05-30 16:55:23 -0400

    taskmand: 	used astyle to convert *.[ch] to awn core coding style

--- src/taskmand/taskmand.h
+++ src/taskmand/taskmand.h
@@ -16,30 +16,30 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA.
  *
 */
- 
- 
+
+
 #ifndef __TASKMANAGER_APPLET
- 
+
 #define __TASKMANAGER_APPLET
 
 typedef struct
 {
-	GObject parent;
-	DBusGConnection *connection;
+  GObject parent;
+  DBusGConnection *connection;
 } Taskmand;
 
 typedef struct
 {
-	GObjectClass parent_class;
+  GObjectClass parent_class;
 } TaskmandClass;
 
 static void taskmand_init(Taskmand *server);
 static void taskmand_class_init(TaskmandClass *class);
 
 gboolean taskmand_launcher_register(Taskmand *obj, gchar *uid, GError **error);
-gboolean taskmand_inform_task_ownership(Taskmand *obj, gchar *uid,gchar *pid, gchar *request,gchar ** response,GError **error);
+gboolean taskmand_inform_task_ownership(Taskmand *obj, gchar *uid, gchar *pid, gchar *request, gchar ** response, GError **error);
 gboolean taskmand_launcher_unregister(Taskmand *obj, gchar *uid, GError **error);
 gboolean taskmand_launcher_position(Taskmand *obj, gchar *uid, GError **error);
-gboolean taskmand_return_xid(Taskmand *obj, gchar *uid,gchar *id, GError **error);
+gboolean taskmand_return_xid(Taskmand *obj, gchar *uid, gchar *id, GError **error);
 
-#endif 
+#endif

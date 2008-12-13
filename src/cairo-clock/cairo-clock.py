bb6f02ae693dd44ad726faab8655624fab600ac7
onox <denkpadje@gmail.com>
2008-12-13 23:37:08 +0100

    * src/cairo-clock/cairo-clock.py:     - Code restructuring (to make it easier for the Locations plug-in to draw more clocks)

--- src/cairo-clock/cairo-clock.py
+++ src/cairo-clock/cairo-clock.py
@@ -309,7 +309,59 @@ class ClockUpdater:
         return True
 
     def load_theme(self):
-        self.__clock.load_theme()
+        provider = AnalogClockThemeProvider(self.default_values["theme"])
+        self.__clock.load_theme(provider)
+
+
+class AnalogClockThemeProvider:
+
+    """Provides handles of SVG files that are part of the theme.
+
+    The librsvg handles can be used to be rendered to a C{cairo.Context}.
+
+    """
+
+    def __init__(self, theme_name):
+        """Load the necessary SVG files of the specified theme.
+
+        """
+        self.__theme_name = theme_name
+
+        theme = self.__get_theme_dir(theme_name)
+        get_theme = lambda name: rsvg.Handle(os.path.join(theme, name))
+
+        # Background
+        self.drop_shadow = get_theme('clock-drop-shadow.svg')
+        self.face = get_theme('clock-face.svg')
+        self.marks = get_theme('clock-marks.svg')
+
+        # Foreground
+        self.face_shadow = get_theme('clock-face-shadow.svg')
+        self.glass = get_theme('clock-glass.svg')
+        self.frame = get_theme('clock-frame.svg')
+
+        # Shadows of hands
+        self.hour_hand_shadow = get_theme('clock-hour-hand-shadow.svg')
+        self.minute_hand_shadow = get_theme('clock-minute-hand-shadow.svg')
+        self.second_hand_shadow = get_theme('clock-second-hand-shadow.svg')
+
+        # Hands
+        self.hour_hand = get_theme('clock-hour-hand.svg')
+        self.minute_hand = get_theme('clock-minute-hand.svg')
+        self.second_hand = get_theme('clock-second-hand.svg')
+
+    def __get_theme_dir(self, theme):
+        theme_dirs = (cairo_clock_themes_dir, default_themes_dir)
+
+        for theme_dir in theme_dirs:
+            path = os.path.join(theme_dir, theme)
+            if os.path.isdir(path):
+                return path
+
+        raise RuntimeError("Did not find path to theme '" + theme + "'")
+
+    def get_name(self):
+        return self.__theme_name
 
 
 class AnalogClock:
@@ -322,72 +374,9 @@ class AnalogClock:
         self.applet = clock_updater.applet
         self.default_values = clock_updater.default_values
 
-    def load_theme(self):
-        """Load the necessary SVG files of the specified theme.
-
-        """
-        theme = self.get_theme_dir(self.default_values["theme"])
-
-        get_theme = lambda name: rsvg.Handle(os.path.join(theme, name))
-
-        # Background
-        self.clock_drop_shadow = get_theme('clock-drop-shadow.svg')
-        self.clock_face = get_theme('clock-face.svg')
-        self.clock_marks = get_theme('clock-marks.svg')
-
-        # Foreground
-        self.clock_face_shadow = get_theme('clock-face-shadow.svg')
-        self.clock_glass = get_theme('clock-glass.svg')
-        self.clock_frame = get_theme('clock-frame.svg')
-
-        # Shadows of hands
-        self.clock_hour_hand_shadow = get_theme('clock-hour-hand-shadow.svg')
-        self.clock_minute_hand_shadow = get_theme('clock-minute-hand-shadow.svg')
-        self.clock_second_hand_shadow = get_theme('clock-second-hand-shadow.svg')
-
-        # Hands
-        self.clock_hour_hand = get_theme('clock-hour-hand.svg')
-        self.clock_minute_hand = get_theme('clock-minute-hand.svg')
-        self.clock_second_hand = get_theme('clock-second-hand.svg')
-
+    def load_theme(self, provider):
+        self.__theme = provider
         self.__previous_state = None
-
-    def get_theme_dir(self, theme):
-        theme_dirs = (cairo_clock_themes_dir, default_themes_dir)
-
-        for theme_dir in theme_dirs:
-            path = os.path.join(theme_dir, theme)
-            if os.path.isdir(path):
-                return path
-
-        raise RuntimeError("Did not find path to theme '" + theme + "'")
-
-    def create_scaled_surface(self, source_surface, height):
-        surface = source_surface.create_similar(cairo.CONTENT_COLOR_ALPHA, height, height)
-        context = cairo.Context(surface)
-
-        svg_width, svg_height = map(float, self.clock_face.get_dimension_data()[:2])
-        context.scale(height / svg_width, height / svg_height)
-
-        return surface, context
-
-    def setup_background_foreground(self, source_surface, height):
-        """Create new Cairo surfaces for the background and foreground.
-
-        """
-        self.__background_surface, background_context = self.create_scaled_surface(source_surface, height)
-
-        # Draw the background of the clock
-        self.clock_drop_shadow.render_cairo(background_context)
-        self.clock_face.render_cairo(background_context)
-        self.clock_marks.render_cairo(background_context)
-
-        self.__foreground_surface, foreground_context = self.create_scaled_surface(source_surface, height)
-
-        # Draw the foreground of the clock
-        self.clock_face_shadow.render_cairo(foreground_context)
-        self.clock_glass.render_cairo(foreground_context)
-        self.clock_frame.render_cairo(foreground_context)
 
     def draw_clock(self):
         """Render the SVGs on a Cairo surface and uses it as the applet's icon.
@@ -399,19 +388,74 @@ class AnalogClock:
         height = self.applet.get_height()
         show_seconds_hand = self.default_values["show-seconds-hand"]
 
-        new_state = (show_seconds_hand, height, self.default_values["theme"], hours, minutes)
+        new_state = (show_seconds_hand, height, self.__theme.get_name(), hours, minutes)
         if not show_seconds_hand and self.__previous_state == new_state:
             return
+
+        if self.__previous_state is None or height != self.__previous_state[1]:
+            self.__base_clock = BaseAnalogClock(self.__theme, height)
+
+        self.__previous_state = new_state
 
         surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, height, height)
         context = cairo.Context(surface)
 
-        if self.__previous_state is None or height != self.__previous_state[1]:
-            self.setup_background_foreground(surface, height)
+        if not show_seconds_hand:
+            seconds = None
+        self.__base_clock.draw_clock(context, height, hours, minutes, seconds)
 
-        self.__previous_state = new_state
+        self.applet.icon.set(context)
 
-        svg_width, svg_height = map(float, self.clock_face.get_dimension_data()[:2])
+
+class BaseAnalogClock:
+
+    """Used to draw a clock to a C{cairo.Context} using pre-rendered
+    Cairo surfaces of the background and foreground of the analog clock.
+
+    Having pre-rendered the background and foreground means those surfaces
+    don't have to be rendered everytime a clock is drawn, saving CPU cycles.
+
+    """
+
+    def __init__(self, theme_provider, height):
+        """Given a height, create a C{BaseAnalogClock} using the theme
+        provided by given provider.
+
+        The base analog clock will contain a background and foreground
+        Cairo surface that has been constructed using this theme and height.
+
+        """
+        self.__theme = theme_provider
+
+        source_surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, height, height)
+
+        self.__background_surface, background_context = self.__create_scaled_surface(source_surface, height)
+
+        # Draw the background of the clock
+        self.__theme.drop_shadow.render_cairo(background_context)
+        self.__theme.face.render_cairo(background_context)
+        self.__theme.marks.render_cairo(background_context)
+
+        self.__foreground_surface, foreground_context = self.__create_scaled_surface(source_surface, height)
+
+        # Draw the foreground of the clock
+        self.__theme.face_shadow.render_cairo(foreground_context)
+        self.__theme.glass.render_cairo(foreground_context)
+        self.__theme.frame.render_cairo(foreground_context)
+
+        del source_surface
+
+    def __create_scaled_surface(self, source_surface, height):
+        surface = source_surface.create_similar(cairo.CONTENT_COLOR_ALPHA, height, height)
+        context = cairo.Context(surface)
+
+        svg_width, svg_height = map(float, self.__theme.face.get_dimension_data()[:2])
+        context.scale(height / svg_width, height / svg_height)
+
+        return surface, context
+
+    def draw_clock(self, context, height, hours, minutes, seconds):
+        svg_width, svg_height = map(float, self.__theme.face.get_dimension_data()[:2])
 
         # Draw the background of the clock
         context.set_operator(cairo.OPERATOR_SOURCE)
@@ -429,23 +473,23 @@ class AnalogClock:
         # Draw the hour hand
         context.save()
         context.rotate((360/12) * (hours+9+(minutes/60.0)) * (math.pi/180))
-        self.clock_hour_hand_shadow.render_cairo(context)
-        self.clock_hour_hand.render_cairo(context)
+        self.__theme.hour_hand_shadow.render_cairo(context)
+        self.__theme.hour_hand.render_cairo(context)
         context.restore()
 
         # Draw the minute hand
         context.save()
         context.rotate((360/60) * (minutes+45) * (math.pi/180))
-        self.clock_minute_hand_shadow.render_cairo(context)
-        self.clock_minute_hand.render_cairo(context)
+        self.__theme.minute_hand_shadow.render_cairo(context)
+        self.__theme.minute_hand.render_cairo(context)
         context.restore()
 
         # Draw the second hand if configured to do so
-        if show_seconds_hand:
+        if seconds is not None:
             context.save()
             context.rotate((360/60) * (seconds+45) * (math.pi/180))
-            self.clock_second_hand_shadow.render_cairo(context)
-            self.clock_second_hand.render_cairo(context)
+            self.__theme.second_hand_shadow.render_cairo(context)
+            self.__theme.second_hand.render_cairo(context)
             context.restore()
 
         context.restore()
@@ -456,8 +500,6 @@ class AnalogClock:
         # Draw foreground of the clock
         context.set_source_surface(self.__foreground_surface)
         context.paint()
-
-        self.applet.icon.set(context)
 
 
 if __name__ == "__main__":

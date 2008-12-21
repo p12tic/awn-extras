5fe0f51b15ce21982f30ac308f5ba790c40f57b4
onox <denkpadje@gmail.com>
2008-12-21 01:30:11 +0100

    * src/thinkhdaps/thinkhdaps.py:     - PEP 8 changes     - Small refactoring

--- src/thinkhdaps/thinkhdaps.py
+++ src/thinkhdaps/thinkhdaps.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 # Copyright (C) 2008  onox <denkpadje@gmail.com>
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, version 3 of the License.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -24,7 +24,7 @@ from gtk import gdk
 from awn.extras import AWNLib
 
 applet_name = "ThinkHDAPS"
-applet_version = "0.2.8"
+applet_version = "0.3.1"
 applet_description = "Applet that shows the status of HDAPS"
 
 # Interval in milliseconds between two successive status checks
@@ -50,93 +50,92 @@ file_icon_error = os.path.join(image_dir, "thinkhdaps-error.svg")
 class ThinkHDAPSApplet:
 
     """Applet that shows the status of HDAPS.
-    
+
     """
+
+    __hdaps_device = None
+
+    __was_paused = False
+    __error_occurred = False
 
     def check_status_cb(self, this):
         """Check the status the hard disk monitored by HDAPS and change
         the applet's icon if necessary,
-        
+
         """
         try:
-            self.was_paused = self.paused
-            self.paused = int(open("/sys/block/" + self.hdaps_device + "/queue/protect").readline())
-            
+            paused = int(open("/sys/block/" + self.__hdaps_device + "/queue/protect").readline())
+
             # Change icon if status has changed
-            if self.paused != self.was_paused or self.error_occurred:
-                if self.paused:
+            if paused != self.__was_paused or self.__error_occurred:
+                if paused:
                     self.applet.icon.set(self.icon_paused, True)
                 else:
                     self.applet.icon.set(self.icon_running, True)
-            
-            if self.error_occurred:
-                self.error_occurred = False
+
+            if self.__error_occurred:
+                self.__error_occurred = False
                 self.applet.title.set(hdaps_short_description + " active")
+
+            self.__was_paused = paused
         except IOError:
-            if not self.error_occurred:
-                self.error_occurred = True
-                
-                # Just load the icon here, it's not gonna be used often
-                height = self.applet.get_height()
-                icon_error = gdk.pixbuf_new_from_file_at_size(file_icon_error, height, height)
-                
-                self.applet.icon.set(icon_error, True)
-                
-                if self.hdaps_device:
-                    self.applet.title.set(hdaps_short_description + " disabled")
-                else:
-                    self.applet.title.set("No hard disk found")
-        
+            if not self.__error_occurred:
+                self.__error_occurred = True
+
+                self.set_error_icon()
+                self.applet.title.set(hdaps_short_description + " disabled")
+
         return True
-    
+
     def __init__(self, applet):
         self.applet = applet
-        
+
         self.setup_icon()
-        
+
         # Set the applet's current icon
         applet.icon.set(self.icon_running, True)
-        
+
         applet.title.set(hdaps_short_description + " active")
-        
-        self.paused = 0
-        self.was_paused = False
-        self.error_occurred = False
-        
-        self.hdaps_device = ""
+
         for device in devices:
             if os.path.isdir(os.path.join("/sys/block", device)):
-                self.hdaps_device = device
+                self.__hdaps_device = device
                 break
-        
+
         applet.connect("height-changed", self.height_changed_cb)
-        
-        # Set up a timer for checking the status of HDAPS if a hard disk has been found
-        if self.hdaps_device:
+
+        if self.__hdaps_device is not None:
             gobject.timeout_add(check_status_interval, self.check_status_cb, self)
         else:
-            self.check_status_cb(self)
-    
+            self.set_error_icon()
+            self.applet.title.set("No hard disk found")
+
     def height_changed_cb(self, widget, event):
         """Update the applet's icon, because the height of the panel
         has changed.
-        
+
         """
         self.setup_icon()
-        
+
         # Toggle the flag to the wrong state to trigger the update of the icon
-        self.error_occurred = not self.error_occurred
-        
+        self.__error_occurred = not self.__error_occurred
+
         # Check the status to update the applet's icon
         self.check_status_cb(self)
-    
+
     def setup_icon(self):
         """Load the images that are going to be used as the applet's icon.
-        
+
         """
         height = self.applet.get_height()
         self.icon_running = gdk.pixbuf_new_from_file_at_size(file_icon_running, height, height)
         self.icon_paused = gdk.pixbuf_new_from_file_at_size(file_icon_paused, height, height)
+
+    def set_error_icon(self):
+        height = self.applet.get_height()
+        icon_error = gdk.pixbuf_new_from_file_at_size(file_icon_error, height, height)
+
+        self.applet.icon.set(icon_error, True)
 
 
 if __name__ == "__main__":

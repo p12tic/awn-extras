2e603b793ece0da8beef135beead01da33ddf6ab
Andrewsomething <a.starr.b@gmail.com>
2009-01-16 12:07:28 -0500

    * comic/getgarfield.py   - Update for new url and clean up some cruft   - Thanks to MathieuF (LP: #297753)

--- src/comic/getgarfield.py
+++ src/comic/getgarfield.py
@@ -14,7 +14,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 ################################################################
-# get_bornloser_strips.py -- fetch strips of last N days
+# getgarfield.py -- fetch strips of last N days
 
 ################################################################
 # BEGIN configuration
@@ -36,40 +36,21 @@ from datetime import datetime, timedelta
 if len(sys.argv) > 1:
     number_of_days = int(sys.argv[1])
 
-pattern = re.compile('ga\\d+\\.gif')
-pattern2 = re.compile('ga\\d+\\.jpg')
-temp1 = 'http://www.gocomics.com/garfield/%s/%s/%s/'
-temp2 = 'http://images.ucomics.com/comics/ga/%s/%s'
+temp = 'http://picayune.uclick.com/comics/ga/%s/ga%s.%s'
 
 date = datetime.today()
 one_day = timedelta(1)
 
-filename = None
-
 for i in range(number_of_days):
-    url = temp1 % (date.strftime('%Y'),date.strftime('%m'),date.strftime('%d'))
-    #print '? %s' % (url)
-    fil = urllib.urlopen(url)
-    for line in fil:
-        match = pattern.search(line)
-        if match != None:
-            filename = match.group()
-            break
-    	else:
-	    match2 = pattern2.search(line)
-	    if match2 != None:
-	        filename = match2.group()
-                break
-    fil.close()
 
-    if filename != None:
-        url = temp2 % (date.strftime('%Y'),filename)
-        #print '+ %s' % (url)
-        fil = urllib.urlopen(url)
-        diskfile = file(path_prefix + 'dilbert.gif', 'w')
-        diskfile.write(fil.read())
-        fil.close()
-        diskfile.close()
+    giforjpg = 'gif'
+      
+    url = temp % (date.strftime('%Y'),date.strftime('%y%m%d'),giforjpg)
+    print '+ %s' % (url)
+    fil = urllib.urlopen(url)
+    diskfile = file(path_prefix + 'dilbert.gif', 'w')
+    diskfile.write(fil.read())
+    fil.close()
+    diskfile.close()
 
     date = date - one_day
-    filename = None

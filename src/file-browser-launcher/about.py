64019c0ba357ad4a539286ea685eee26aacf8825
Matt <sharkbaitbobby@gmail.com>
2009-02-21 15:33:06 -0500

    * file-browser-launcher:   -Fix networking stuff   -Add "Connect to server"   -Make Filesystem a separate option   -Fix up about   -Code cleanup and optimization

--- src/file-browser-launcher/about.py
+++ src/file-browser-launcher/about.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2008 sharkbaitbobby <sharkbaitbobby+awn@gmail.com>
+# Copyright (c) 2009 sharkbaitbobby <sharkbaitbobby+awn@gmail.com>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -26,6 +26,7 @@ pygtk.require('2.0')
 import gtk
 import gettext
 import locale
+import os
 
 from awn.extras import defs
 
@@ -34,17 +35,25 @@ gettext.bindtextdomain(APP, defs.GETTEXTDIR)
 gettext.textdomain(APP)
 _ = gettext.gettext
 
+def url_hook(about, url):
+  os.system('xdg-open %s &' % url)
+
 #Simple about dialog window
 class About:
   def __init__(self):
+    gtk.about_dialog_set_url_hook(url_hook)
     win = gtk.AboutDialog()
     win.set_name(_("File Browser Launcher"))
-    win.set_copyright('Copyright 2009 sharkbaitbobby <sharkbaitbobby+awn@gmail.com>')
+    win.set_copyright('Copyright 2009 sharkbaitbobby')
     win.set_authors(['sharkbaitbobby <sharkbaitbobby+awn@gmail.com>'])
     win.set_comments(_("A customizable launcher for browsing your files."))
     win.set_license('This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA.')
     win.set_wrap_license(True)
     win.set_documenters(["sharkbaitbobby <sharkbaitbobby+awn@gmail.com>"])
-    win.set_artists(["GTK Stock Icons","Human Icon for \"folder\""])
+    win.set_artists(["GTK Stock Icons", "Human Icon for \"folder\""])
+    win.set_website('http://wiki.awn-project.org/File_Browser_Launcher')
+    win.set_website_label('wiki.awn-project.org')
+    win.set_logo_icon_name('stock_folder')
+    win.set_icon_name('stock_folder')
     win.run()
     win.destroy()

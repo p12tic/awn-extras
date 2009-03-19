77d33284e1f7c54a4ead4046f5fa94796b5d81a9
onox <denkpadje@gmail.com>
2009-03-19 21:31:31 +0100

    Animal Farm: rstrip text and put 6px of padding around it

--- src/animal-farm/animal-farm.py
+++ src/animal-farm/animal-farm.py
@@ -42,29 +42,34 @@ command = "fortune"
 class AnimalFarmApplet:
 
     """Applet that displays fortune messages.
-    
+
     """
-    
+
     def __init__(self, applet):
         self.applet = applet
-        
+
         self.iconname = self.previous_iconname = None
         self.set_icon()
-        
+
         self.setup_main_dialog()
-        
+
         applet.connect_size_changed(self.refresh_icon)
-    
+
     def setup_main_dialog(self):
         self.dialog = self.applet.dialog.new("fortune-dialog")
-        
+
+        alignment = gtk.Alignment()
+        padding = 6
+        alignment.set_padding(padding, padding, padding, padding)
+        self.dialog.add(alignment)
+
         self.label = gtk.Label()
-        self.dialog.add(self.label)
+        alignment.add(self.label)
         self.refresh_fortune()
-        
+
         self.applet.connect("button-press-event", self.button_press_event_cb)
         self.dialog.connect("focus-out-event", self.dialog_focus_out_cb)
-    
+
     def button_press_event_cb(self, widget, event):
         if event.button == 1:
             if self.dialog.is_active():
@@ -75,28 +80,28 @@ class AnimalFarmApplet:
             self.refresh_fortune()
             if not self.dialog.is_active():
                 self.applet.dialog.toggle("fortune-dialog", "show")
-    
+
     def dialog_focus_out_cb(self, dialog, event):
         self.set_icon()
         self.refresh_fortune()
-    
+
     def set_icon(self):
         files = [i for i in os.listdir(images_dir) if i.endswith('.svg') and i != self.iconname and i != self.previous_iconname]
-        
+
         self.previous_iconname = self.iconname
         self.iconname = files[random.randint(0, len(files) - 1)]
-        
+
         self.refresh_icon()
-    
+
     def refresh_icon(self):
         self.applet.icon.file(os.path.join(images_dir, self.iconname), size=awnlib.Icon.APPLET_SIZE)
-    
+
     def refresh_fortune(self):
         try:
             text = subprocess.Popen(command, stdout=subprocess.PIPE).communicate()[0]
         except OSError:
             text = "Error executing \"" + command + "\"; make sure it is in your path and executable."
-        self.label.set_text(text)
+        self.label.set_text(text.rstrip())
 
 
 if __name__ == "__main__":

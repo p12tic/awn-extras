634f3b28b58ad71470e69fc5b0f89baa89ceb3fa
R. Cryderman <rcryderman@gmail.com>
2009-05-05 22:48:14 -0400

    * src/sysmon/graph.c: * src/sysmon/graph.h:
    	This will be the base class for rendering a graph to a cairo surface.

---
+++ src/sysmon/graph.c
@@ -0,0 +1,87 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor Boston, MA 02110-1301,  USA
+ */
+
+/* awn-graph.c */
+
+#include "graph.h"
+
+G_DEFINE_TYPE (AwnGraph, awn_graph, G_TYPE_OBJECT)
+
+#define GET_PRIVATE(o) \
+  (G_TYPE_INSTANCE_GET_PRIVATE ((o), AWN_TYPE_GRAPH, AwnGraphPrivate))
+
+typedef struct _AwnGraphPrivate AwnGraphPrivate;
+
+struct _AwnGraphPrivate {
+    int dummy;
+};
+
+static void
+awn_graph_get_property (GObject *object, guint property_id,
+                              GValue *value, GParamSpec *pspec)
+{
+  switch (property_id) {
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+  }
+}
+
+static void
+awn_graph_set_property (GObject *object, guint property_id,
+                              const GValue *value, GParamSpec *pspec)
+{
+  switch (property_id) {
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+  }
+}
+
+static void
+awn_graph_dispose (GObject *object)
+{
+  G_OBJECT_CLASS (awn_graph_parent_class)->dispose (object);
+}
+
+static void
+awn_graph_finalize (GObject *object)
+{
+  G_OBJECT_CLASS (awn_graph_parent_class)->finalize (object);
+}
+
+static void
+awn_graph_class_init (AwnGraphClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  g_type_class_add_private (klass, sizeof (AwnGraphPrivate));
+
+  object_class->get_property = awn_graph_get_property;
+  object_class->set_property = awn_graph_set_property;
+  object_class->dispose = awn_graph_dispose;
+  object_class->finalize = awn_graph_finalize;
+}
+
+static void
+awn_graph_init (AwnGraph *self)
+{
+}
+
+AwnGraph*
+awn_graph_new (void)
+{
+  return g_object_new (AWN_TYPE_GRAPH, NULL);
+}
+

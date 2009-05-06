453c71b85b5bb3c961719081d7c1454fd61d2540
R. Cryderman <rcryderman@gmail.com>
2009-05-05 20:20:05 -0400

    * src/sysmon/Makefile.am: * src/sysmon/applet.c: * src/sysmon/sysmon.c: * src/sysmon/sysmon.h: * src/sysmon/sysmonicon.c: * src/sysmon/sysmonicon.h:
    	Further early work on sysmon applet rewrite.

--- src/sysmon/sysmon.c
+++ src/sysmon/sysmon.c
@@ -1,3 +1,19 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor Boston, MA 02110-1301,  USA
+ */
+
 /* awn-sysmon.c */
 
 #include "sysmon.h"
@@ -71,7 +87,7 @@ awn_sysmon_init (AwnSysmon *sysmon)
   priv->box = awn_icon_box_new_for_applet (AWN_APPLET (sysmon));
   gtk_container_add (GTK_CONTAINER (sysmon), priv->box);
   gtk_widget_show (priv->box);
-  icon = awn_sysmonicon_new ();
+  icon = awn_CPUicon_new ();
   pixbuf = gtk_icon_theme_load_icon (gtk_icon_theme_get_default (), 
                                      "gnome-system-monitor",
                                      40, 

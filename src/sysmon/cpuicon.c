791c7d3836d028deac036648693689df1d30a9f5
R. Cryderman <rcryderman@gmail.com>
2009-05-05 20:21:30 -0400

    * src/sysmon/cpuicon.c: * src/sysmon/cpuicon.h:
    	Initial skeleton of cpu icon.

---
+++ src/sysmon/cpuicon.c
@@ -0,0 +1,88 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor Boston, MA 02110-1301,  USA
+ */
+ 
+ 
+ /* awn-CPUicon.c */
+
+#include "cpuicon.h"
+
+G_DEFINE_TYPE (AwnCPUicon, awn_CPUicon, AWN_TYPE_SYSMONICON)
+
+#define GET_PRIVATE(o) \
+  (G_TYPE_INSTANCE_GET_PRIVATE ((o), AWN_TYPE_CPUICON, AwnCPUiconPrivate))
+
+typedef struct _AwnCPUiconPrivate AwnCPUiconPrivate;
+
+struct _AwnCPUiconPrivate {
+    int dummy;
+};
+
+static void
+awn_CPUicon_get_property (GObject *object, guint property_id,
+                              GValue *value, GParamSpec *pspec)
+{
+  switch (property_id) {
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+  }
+}
+
+static void
+awn_CPUicon_set_property (GObject *object, guint property_id,
+                              const GValue *value, GParamSpec *pspec)
+{
+  switch (property_id) {
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+  }
+}
+
+static void
+awn_CPUicon_dispose (GObject *object)
+{
+  G_OBJECT_CLASS (awn_CPUicon_parent_class)->dispose (object);
+}
+
+static void
+awn_CPUicon_finalize (GObject *object)
+{
+  G_OBJECT_CLASS (awn_CPUicon_parent_class)->finalize (object);
+}
+
+static void
+awn_CPUicon_class_init (AwnCPUiconClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  g_type_class_add_private (klass, sizeof (AwnCPUiconPrivate));
+
+  object_class->get_property = awn_CPUicon_get_property;
+  object_class->set_property = awn_CPUicon_set_property;
+  object_class->dispose = awn_CPUicon_dispose;
+  object_class->finalize = awn_CPUicon_finalize;
+}
+
+static void
+awn_CPUicon_init (AwnCPUicon *self)
+{
+}
+
+GtkWidget*
+awn_CPUicon_new (void)
+{
+  return g_object_new (AWN_TYPE_CPUICON, NULL);
+}
+

9bb8fc3d7eceb5781f4f0b7be94a8076b43d1d5a
R. Cryderman <rcryderman@gmail.com>
2009-05-15 13:11:05 -0400

    * src/sysmon/circlegraph.c:
    	Adjusted appearance of circlegraph.

--- src/sysmon/circlegraph.c
+++ src/sysmon/circlegraph.c
@@ -168,8 +168,8 @@ static void _awn_circlegraph_render_to_context(AwnGraph * graph,
   usage = (priv->current_val + priv->prev_val) / 200.0;
   pat = cairo_pattern_create_radial (128,  128, 0,
                                      128,  128, 128.0);
-  cairo_pattern_add_color_stop_rgba (pat, 0, 1, 0, 0, 1 );
-  cairo_pattern_add_color_stop_rgba (pat, 1, 0, 1 *(1-usage), 0, 1);
+  cairo_pattern_add_color_stop_rgba (pat, 0, sin (M_PI/2 *usage), (1-sin (M_PI/2 *usage))/3, 0, 1 );
+  cairo_pattern_add_color_stop_rgba (pat, 1, usage/2, 1 *(1-usage), 0, 1);
   cairo_set_source (cr, pat);
   cairo_arc (cr, 128.0, 128.0, 120, 0, 2 * M_PI);
   cairo_fill (cr);

095d56a8c833176f1c13e9123d31ce825134847b
R. Cryderman <rcryderman@gmail.com>
2009-05-18 12:22:01 -0400

    * src/sysmon/bargraph.c: * src/sysmon/bargraph.h:
    	Skeleton of bargraph.

---
+++ src/sysmon/bargraph.c
@@ -0,0 +1,87 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor Boston, MA 02110-1301,  USA
+ */
+ 
+ /* awn-bargraph.c */
+
+#include "bargraph.h"
+
+G_DEFINE_TYPE (AwnBarGraph, awn_bargraph, AWN_TYPE_BARGRAPH)
+
+#define GET_PRIVATE(o) \
+  (G_TYPE_INSTANCE_GET_PRIVATE ((o), AWN_TYPE_BARGRAPH, AwnBarGraphPrivate))
+
+typedef struct _AwnBarGraphPrivate AwnBarGraphPrivate;
+
+struct _AwnBarGraphPrivate {
+    int dummy;
+};
+
+static void
+awn_bargraph_get_property (GObject *object, guint property_id,
+                              GValue *value, GParamSpec *pspec)
+{
+  switch (property_id) {
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+  }
+}
+
+static void
+awn_bargraph_set_property (GObject *object, guint property_id,
+                              const GValue *value, GParamSpec *pspec)
+{
+  switch (property_id) {
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+  }
+}
+
+static void
+awn_bargraph_dispose (GObject *object)
+{
+  G_OBJECT_CLASS (awn_bargraph_parent_class)->dispose (object);
+}
+
+static void
+awn_bargraph_finalize (GObject *object)
+{
+  G_OBJECT_CLASS (awn_bargraph_parent_class)->finalize (object);
+}
+
+static void
+awn_bargraph_class_init (AwnBarGraphClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  g_type_class_add_private (klass, sizeof (AwnBarGraphPrivate));
+
+  object_class->get_property = awn_bargraph_get_property;
+  object_class->set_property = awn_bargraph_set_property;
+  object_class->dispose = awn_bargraph_dispose;
+  object_class->finalize = awn_bargraph_finalize;
+}
+
+static void
+awn_bargraph_init (AwnBarGraph *self)
+{
+}
+
+AwnBarGraph*
+awn_bargraph_new (void)
+{
+  return g_object_new (AWN_TYPE_BARGRAPH, NULL);
+}
+
---
+++ src/sysmon/bargraph.h
@@ -0,0 +1,58 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor Boston, MA 02110-1301,  USA
+ */
+ 
+ /* awn-bargraph.h */
+
+#ifndef _AWN_BARGRAPH
+#define _AWN_BARGRAPH
+
+#include <glib-object.h>
+#include "graph.h"
+
+G_BEGIN_DECLS
+
+#define AWN_TYPE_BARGRAPH awn_bargraph_get_type()
+
+#define AWN_BARGRAPH(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST ((obj), AWN_TYPE_BARGRAPH, AwnBarGraph))
+
+#define AWN_BARGRAPH_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_CAST ((klass), AWN_TYPE_BARGRAPH, AwnBarGraphClass))
+
+#define AWN_IS_BARGRAPH(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE ((obj), AWN_TYPE_BARGRAPH))
+
+#define AWN_IS_BARGRAPH_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_TYPE ((klass), AWN_TYPE_BARGRAPH))
+
+#define AWN_BARGRAPH_GET_CLASS(obj) \
+  (G_TYPE_INSTANCE_GET_CLASS ((obj), AWN_TYPE_BARGRAPH, AwnBarGraphClass))
+
+typedef struct {
+  AwnGraph parent;
+} AwnBarGraph;
+
+typedef struct {
+  AwnGraphClass parent_class;
+} AwnBarGraphClass;
+
+GType awn_bargraph_get_type (void);
+
+AwnBarGraph* awn_bargraph_new (void);
+
+G_END_DECLS
+
+#endif /* _AWN_BARGRAPH */

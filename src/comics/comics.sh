734a4fa6b007984e5dad4944f7437a7eb0d00f72
Moses Palmér <moses@moses-desktop>
2009-06-07 18:42:12 +0200

    Added comics.sh. This file will launch main.py if it not running, otherwise it will write commands to the command pipe.  * This allows sending commands without having to launch a separate Python process every time.

---
+++ src/comics/comics.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Mimics the behaviour of main.py, without the overhead of having to launch a
+# Python process just to write to a named pipe.
+
+COMMAND_PIPE="$HOME/.config/awn/applets/comics/commands"
+CLEAN_COMMAND="clean"
+EXIT_COMMAND="exit"
+
+if [ "x$1" = "x$CLEAN_COMMAND" ]; then
+	# The clean command is a special command to this script only
+	rm "$COMMAND_PIPE"
+	exit
+fi
+
+if [ -p "$COMMAND_PIPE" ]; then
+	# If the command pipe exists, we write to it
+	echo $@ >>"$COMMAND_PIPE"
+elif [ -n "x$1" = "x$EXIT_COMMAND" ]; then
+	# Otherwise we start the application in the background
+	python main.py $@ &
+fi
+

d6fa946f7ed07a67f2960f5a737b2b261cc97b3b
onox <denkpadje@gmail.com>
2009-06-18 19:58:44 +0200

    Quit-Log Out Applet: add support for lock screen, log out, and shutdown via GNOME's SessionManager and ScreenSaver, killing the stupid text box in the preference window

--- src/quit-applet/quit-applet.py
+++ src/quit-applet/quit-applet.py
@@ -13,13 +13,9 @@
 # Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public
-# License along with this library; if not, write to the
-# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-# Boston, MA 02111-1307, USA.
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
 import commands
-import os
-import subprocess
 
 import pygtk
 pygtk.require("2.0")
@@ -27,85 +23,106 @@ import gtk
 
 from awn.extras import awnlib
 
-try:
-    import dbus
-except ImportError:
-    dbus = None
+import dbus
 
 applet_name = "Quit-Log Out"
 applet_version = "0.3.3"
-applet_description = "An applet to exit or log out of your session"
+applet_description = "An applet to lock your screen, log out of your session, or shut down the system"
 
-# Themed logo of the applet, used as the applet's icon and shown in the GTK About dialog
+# Themed logo of the applet, shown in the GTK About dialog
 applet_logo = "application-exit"
+
+left_click_actions = ("Lock Screen", "Log Out", "Shut Down")
 
 
 class QuitLogOutApplet:
 
-    """An applet to exit or log out of your session.
+    """An applet to lock your screen, log out of your session, or shut
+    down the system.
 
     """
 
     def __init__(self, applet):
         self.applet = applet
 
-        applet.tooltip.set("Log Out " + commands.getoutput("/usr/bin/whoami") + "...")
+        self.setup_context_menu()
 
-        self.logout_cb = None
+        # Initialize tooltip and icon
+        self.refresh_tooltip_icon_cb(self.settings["left-click-action"])
 
-        if dbus is not None:
-            try:
-                bus = dbus.SessionBus()
-                names = bus.list_names()
-                if "org.gnome.SessionManager" in names and "org.freedesktop.PowerManagement" in names:
-                    proxy = bus.get_object("org.freedesktop.PowerManagement", "/org/freedesktop/PowerManagement")
-                    if dbus.Interface(proxy, "org.freedesktop.PowerManagement").CanShutdown():
-                        self.logout_cb = self.logout_gnome_session_manager
-            except dbus.DBusException, e:
-                print e.message
+        session_bus = dbus.SessionBus()
 
-        if self.logout_cb is None:
-            self.logout_cb = self.logout_shell_command
-            self.setup_context_menu()
+        sm_proxy = session_bus.get_object("org.gnome.SessionManager", "/org/gnome/SessionManager")
+        self.sm_if = dbus.Interface(sm_proxy, "org.gnome.SessionManager")
+
+        ss_proxy = session_bus.get_object("org.gnome.ScreenSaver", "/")
+        self.ss_if = dbus.Interface(ss_proxy, "org.gnome.ScreenSaver")
 
         applet.connect("button-press-event", self.button_press_event_cb)
 
     def button_press_event_cb(self, widget, event):
         if event.button == 1:
-            self.logout_cb()
+            self.left_click_cb()
 
     def setup_context_menu(self):
         pref_dialog = self.applet.dialog.new("preferences")
-        pref_dialog.connect("response", self.pref_dialog_response_cb)
-
         self.setup_dialog_settings(pref_dialog.vbox)
 
+    def refresh_tooltip_icon_cb(self, action_index):
+        action = left_click_actions[action_index]
+
+        if action == "Lock Screen":
+            self.applet.tooltip.set("Lock Screen")
+            self.applet.icon.theme("system-lock-screen")
+        elif action == "Log Out":
+            user_name = commands.getoutput("/usr/bin/whoami")
+            self.applet.tooltip.set("Log Out %s..." % user_name)
+            self.applet.icon.theme("system-log-out")
+        elif action == "Shut Down":
+            self.applet.tooltip.set("Shut Down...")
+            self.applet.icon.theme("system-shutdown")
+
     def setup_dialog_settings(self, vbox):
-        if "logout_command" not in self.applet.settings:
-            self.applet.settings["logout_command"] = "gnome-session-save --kill"
-        self.logout_command = self.applet.settings["logout_command"]
+        defaults = {
+            "left-click-action": (left_click_actions.index("Log Out"), self.refresh_tooltip_icon_cb)
+        }
+        self.settings = self.applet.settings.load_preferences(defaults)
 
-        # TODO replace the entry by a combobox that has the values: GNOME, KDE, ..., custom, etc.
-        self.entry_logout = gtk.Entry()
-        self.entry_logout.set_text(self.logout_command)
+        hbox = gtk.HBox(spacing=12)
+        # Use non-zero border width to align the entry with the close button
+        hbox.set_border_width(5)
+        vbox.add(hbox)
 
-        """ Wrap the entry in a vbox that has a non-zero border width
-        to align the entry with the close button """
-        extra_vbox = gtk.VBox()
-        extra_vbox.set_border_width(5)
-        extra_vbox.add(self.entry_logout)
+        combobox = gtk.combo_box_new_text()
+        for i in left_click_actions:
+            combobox.append_text(i)
+        combobox.set_active(self.settings["left-click-action"])
+        combobox.connect("changed", self.action_changed_cb) 
 
-        vbox.add(extra_vbox)
+        label = gtk.Label("Left click _action:")
+        label.set_use_underline(True)
+        label.set_mnemonic_widget(combobox)
 
-    def pref_dialog_response_cb(self, widget, response):
-        self.applet.settings["logout_command"] = self.logout_command = self.entry_logout.get_text()
+        hbox.add(label)
+        hbox.add(combobox)
 
-    def logout_gnome_session_manager(self):
-        proxy = dbus.SessionBus().get_object("org.gnome.SessionManager", "/org/gnome/SessionManager")
-        dbus.Interface(proxy, "org.gnome.SessionManager").Shutdown()
+    def action_changed_cb(self, widget):
+        self.applet.settings["left-click-action"] = widget.get_active()
 
-    def logout_shell_command(self):
-        subprocess.Popen(self.logout_command, shell=True)
+    def left_click_cb(self):
+        action = left_click_actions[self.settings["left-click-action"]]
+
+        if action == "Lock Screen":
+            try:
+                self.ss_if.Lock()
+            except dbus.DBusException, e:
+                # NoReply exception may occur even while the screensaver did lock the screen
+                if e.get_dbus_name() != "org.freedesktop.DBus.Error.NoReply":
+                    raise
+        elif action == "Log Out":
+            self.sm_if.Logout(0)
+        elif action == "Shut Down":
+            self.sm_if.Shutdown()
 
 
 if __name__ == "__main__":
@@ -113,7 +130,7 @@ if __name__ == "__main__":
         "version": applet_version,
         "description": applet_description,
         "theme": applet_logo,
-        "author": "onox, Randal Barlow",
+        "author": "onox",
         "copyright-year": "2008 - 2009",
         "authors": ["Randal Barlow <im.tehk at gmail.com>", "onox <denkpadje@gmail.com>"]},
         ["settings-per-instance"])

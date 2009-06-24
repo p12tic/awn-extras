f6409459a9131d3df15944956c94a5dc2deb08e3
Michal Hruby <michal.mhr@gmail.com>
2009-06-25 01:01:21 +0200

    * Media-player: Fixed 3rd param for OverlayThemedIcon

--- src/media-player/media_player.py
+++ src/media-player/media_player.py
@@ -43,7 +43,7 @@ class App(awn.AppletSimple):
         self.full_window = None
         self.dialog = awn.Dialog(self)
         self.play_icon = awn.OverlayThemedIcon(self.get_icon(),
-                                               "media-playback-start")
+                                               "media-playback-start", None)
         self.play_icon.props.scale = 0.4
         self.play_icon.props.active = False
         self.add_overlay(self.play_icon)

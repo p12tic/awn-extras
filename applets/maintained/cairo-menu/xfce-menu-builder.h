2a568a39e3f1b6a340fede564bc748758449906f
R. Cryderman <rcryderman@gmail.com>
2009-10-11 16:37:11 -0400

    * applets/maintained/cairo-menu/xfce-menu-builder.c: * applets/maintained/cairo-menu/xfce-menu-builder.h: 	Skeleton xfce menu code.  More or less of copy and past of gnome menu code 	at this point 	Not integrated into build system

---
+++ applets/maintained/cairo-menu/xfce-menu-builder.h
@@ -0,0 +1,13 @@
+#ifndef _XFCE_MENU_BUILDER
+#define _XFCE_MENU_BUILDER
+
+#include <gtk/gtk.h>
+#include "cairo-menu.h"
+
+G_BEGIN_DECLS
+
+GtkWidget * xfce_menu_build (void);
+
+G_END_DECLS
+
+#endif /* _XFCE_MENU_BUILDER */
